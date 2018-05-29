bcec8508c107785e35cafbbb61af298ff5c60c80
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2018-05-29 15:25:34 +0300

    Update firewall.sh

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -2,3 +2,5 @@
 
 /sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8743 &>/dev/null
 /sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080 &>/dev/null
+/sbin/ip6tables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080 &>/dev/null
+/sbin/ip6tables -t nat -D PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8743 &>/dev/null
